d001de1bdbdf003c2b13b70f3c0c6278f187058b
Meryeme boumlik <meryeme.boumlik@gmail.com>
2022-06-27 14:28:10 +0100

    optimisation code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,60 +1,76 @@
 package com.gildedrose;
 
+
 class GildedRose {
     Item[] items;
+    enum levels {
+        Sulfuras {
+            public String toString() {
+                return "Sulfuras, Hand of Ragnaros";
+            }
+        },
+        Backstage {
+            public String toString() {
+                return "Backstage passes to a TAFKAL80ETC concert";
+            }
+        },
+        AGED {
+            public String toString() {
+                return "Aged Brie";
+            }
+        };
+    }
+
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+
+  public void  changequality( Item item){
+      if (item.quality < 50) {
+          item.quality =+ 1;
+      }
+      if (item.quality > 0) {
+          item.quality = item.quality - 1;
+      }
+  }
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+        for (Item item :items) {
+            if (!levels.AGED.equals(item.name) && !levels.Backstage.equals(item.name)){
+                    changequality(item);
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                changequality(item);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                    if (levels.Backstage.equals(item.name)) {
+                        if (item.sellIn < 11) {
+                            changequality(item);
+                        }
+                        if (item.sellIn < 6) {
+                            changequality(item);
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!levels.Sulfuras.equals(item.name)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!levels.AGED.equals(item.name)) {
+                    if (!levels.Backstage.equals(item.name)) {
+                        if (item.quality > 0) {
+                            if (!levels.Sulfuras.equals(item.name)) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    changequality(item);
                 }
             }
         }
---
+++ Java/src/main/java/com/gildedrose/Item1.java
@@ -0,0 +1,12 @@
+package com.gildedrose;
+
+import com.gildedrose.Item;
+
+public class Item1  extends Item {
+    public Item1(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+    public Item1() {
+        super();
+    }
+}
